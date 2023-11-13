e294046319545ac97398ccccb093c5d076810d06
rbrtj <robertjaszczurek@icloud.com>
2023-11-13 22:00:22 +0100

    init with tailwind

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import ServersList from '../components/servers-list/ServersList.vue'
+</script>
+
+<template>
+  <main>
+    <ServersList />
+  </main>
+</template>
