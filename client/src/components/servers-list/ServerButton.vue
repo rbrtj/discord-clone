fc4c474fabfee044d45d02b7c58772bb32502209
rbrtj <robertjaszczurek@icloud.com>
2023-11-13 23:47:25 +0100

    basic server list + server button

---
+++ client/src/components/servers-list/ServerButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ActionTooltip from '../ui/ActionTooltip.vue'
+
+defineProps<{
+  imageUrl: string
+  name: string
+  id: string
+}>()
+const onClick = () => {
+  console.log('clicked')
+}
+console.log(name)
+</script>
+
+<template>
+  <ActionTooltip :content="name">
+    <button :click="onClick" class="relative flex items-center">
+      <div
+        class="relative flex mx-3 h-[48px] w-[48px] rounded-[24px] transition-all overflow-hidden"
+      >
+        <img :src="imageUrl" alt="Channel" />
+      </div>
+    </button>
+  </ActionTooltip>
+</template>
--- client/src/components/servers-list/ServersList.vue
+++ client/src/components/servers-list/ServersList.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ServerButton from './ServerButton.vue'
+const servers = ref([
+  {
+    name: 'M'
+  },
+  {
+    name: 'S1'
+  },
+  {
+    name: 'S2'
+  },
+  {
+    name: 'S3'
+  }
+])
+</script>
+
 <template>
-  <div class="bg-white">
-    <h1 class="text-5xl">Hi there</h1>
+  <div class="fixed left-0 top-0 w-20 bg-[#1e2124] h-full">
+    <div class="pt-6 flex flex-col items-center">
+      <ServerButton name="@me" image-url="discord.png" id="1" />
+      <ul class="pt-4">
+        <li v-for="server in servers" :key="server.name">
+          {{ server.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
